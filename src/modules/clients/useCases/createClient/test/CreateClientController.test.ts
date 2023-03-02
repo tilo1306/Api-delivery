f3b5ef81d1ef71d91af00dd9a03f1dc1b2fae239
Douglas dos Santos Oliveria <doug1306@gmail.com>
2023-03-02 16:45:11 -0300

    feat: end test integration create client

---
+++ src/modules/clients/useCases/createClient/test/CreateClientController.test.ts
@@ -0,0 +1,36 @@
+import request from "supertest";
+
+import { app } from "../../../../../app";
+import { prisma } from "../../../../../database/prismaClient";
+
+describe("Create client controller", () => {
+  const username = "usernameTest";
+  const password = "123456";
+  beforeAll(async () => {
+    await prisma.clients.deleteMany({ where: {} });
+  });
+
+  it("Should register a new client", async () => {
+    const response = await request(app).post("/client").send({
+      username,
+      password,
+    });
+    expect(response.statusCode).toEqual(201);
+  });
+
+  it("should not be able to create a new client with username exists", async () => {
+    await request(app).post("/client/").send({
+      username,
+      password,
+    });
+
+    const response = await request(app).post("/client").send({
+      username,
+      password,
+    });
+
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.message).toEqual("Username already exists");
+  });
+});
